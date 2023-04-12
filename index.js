602cbefa657e39b6920ae4d7dd39a0833ede591d
Lane G Smith <lanegphotography@gmail.com>
2023-04-12 11:10:05 -0700

    Update logic

--- index.js
+++ index.js
@@ -37,13 +37,15 @@ client.on('messageCreate', async function (message) {
         // ignore input from the bot itself
         if (message.author.bot) return;
         // ignore input that contains a * (used to trigger the next function)
-        else if (message.content.toLowerCase().includes('*')) return;
+        else if (message.content.toLowerCase().includes('***')) return;
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
-                { "role": "system", "content": "" },
-                { "role": "user", "content": "" },
-                { "role": "assistant", "content": "" },
+                { "role": "system", "content": "You are a job interviewer interviewing an applicant for a job as a junior javascript web developer. You are looking for an applicant that is proficient in HTML, CSS, Javascript, React, Nodejs, Mongodb, Expressjs, etc. You ask questions and provide short coding challenges and tasks from other real junior web developer interviews. The question subject should be the same as the prompt, if no subject is specified then the subject is random. Give each question a difficulty rating between 1 and 10 written like 1/10, 2/10, etc. Provide questions/challenges/tasks of a specific difficulty when prompted, difficulty is random if not specified." },
+                { "role": "user", "content": "javascript 2/10" },
+                { "role": "assistant", "content": "in the following array which item is at position zero? fruits[apple, banana, orange, pear]" },
+                { "role": "user", "content": "html" },
+                { "role": "assistant", "content": "what does html stand for? 1/10" },
                 { "role": "user", "content": `${message.content}` }
             ]
         });
@@ -60,11 +62,11 @@ client.on('messageCreate', async function (message) {
         // ignore input from the bot itself
         if (message.author.bot) return;
         // ignore input that doesn't contain a *
-        else if (message.content.toLowerCase().includes('*')) {
+        else if (message.content.toLowerCase().includes('***')) {
             const completion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: [
-                    { "role": "system", "content": "Describe the desired AI characteristics and how questions should be answered" },
+                    { "role": "system", "content": "You are a senior javascript web developer teaching a 10-year-old how to do web development. Answer questions in concise and simple terms. Be thorough and provide documentation from MDN or other online resources when necessary." },
                     { "role": "user", "content": "A sample question will go here" },
                     { "role": "assistant", "content": "An answer to the previous question will go here" },
                     { "role": "user", "content": `${responseOne}` }
